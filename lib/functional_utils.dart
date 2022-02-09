db4ca54e1b3c45d3d1b9b594f15f116172322f53
Antoine Duchâteau <ad@taktik.com>
2022-02-09 15:58:03 +0100

    Cleanup

--- lib/functional_utils.dart
+++ lib/functional_utils.dart
@@ -2,18 +2,9 @@
 part of openapi.api;
 
 extension ObjectExt<T> on T {
-  R let<R>(R Function(T that) op) => op(this as T);
+  R let<R>(R Function(T that) op) => op(this);
   T also(void Function(T that) op) {
-    var self = this as T;
-    op(self);
-    return self;
-  }
-}
-
-extension FutureExt<T> on Future<T> {
-  Future<T> also(void Function(Future<T> that) op) {
-    var self = this as Future<T>;
-    op(self);
-    return self;
+    op(this);
+    return this;
   }
 }
