73c80bde7bb4a1627db5eba44ebae873dc11c59b
Antoine Duchâteau <ad@taktik.com>
2022-02-11 18:11:59 +0100

    More filters fixes

--- lib/model/filter/service/service_by_secret_foreign_keys.dart
+++ lib/model/filter/service/service_by_secret_foreign_keys.dart
@@ -2,6 +2,7 @@
 part of icure_dart_sdk.api;
 
 class ServiceBySecretForeignKeys extends AbstractFilterDto<ServiceDto> {
+  ServiceBySecretForeignKeys({this.desc, this.healthcarePartyId, this.patientSecretForeignKeys = const {}});
 
   @override
   String? desc;
