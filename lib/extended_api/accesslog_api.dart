c1b28e8c737ed0405d38853f62f0799a6e35d1a8
Clément Vandendaelen <cv@taktik.com>
2022-02-15 11:16:12 +0100

    Renamed getDecryptedAccessLogWithKeys

--- lib/extended_api/accesslog_api.dart
+++ lib/extended_api/accesslog_api.dart
@@ -50,9 +50,9 @@ extension AccessLogCryptoConfig on CryptoConfig<DecryptedAccessLogDto, AccessLog
   Future<AccessLogDto> encryptAccessLog(String myId, Set<String> delegations, DecryptedAccessLogDto accessLogDto) async {
     final Uuid uuid = Uuid();
 
-    Future<DecryptedAccessLogDto> getEncryptedAccessLog(DecryptedAccessLogDto accessLog) async {
-      if (accessLogDto.encryptionKeys.entries.any((element) => element.value.isNotEmpty)) {
-        return accessLogDto;
+    Future<DecryptedAccessLogDto> getDecryptedAccessLogWithKeys(DecryptedAccessLogDto accessLog) async {
+      if (accessLog.encryptionKeys.entries.any((element) => element.value.isNotEmpty)) {
+        return accessLog;
       } else {
         final String secret = uuid.v4(options: {'rng': UuidUtil.cryptoRNG});
         final Set<String> newDelegations = [...delegations, myId].toSet();
@@ -63,12 +63,14 @@ extension AccessLogCryptoConfig on CryptoConfig<DecryptedAccessLogDto, AccessLog
         final Map<String, String> delegationsKeys = await Map<String, String>.fromEntries(await Future.wait(futureDelegationKeys));
         final Map<String, Set<DelegationDto>> encryptionKeys = Map<String, Set<DelegationDto>>.fromEntries(
             delegationsKeys.entries.map((e) => MapEntry(e.key, [DelegationDto(owner: myId, delegatedTo: e.key, key: e.value, tags: [])].toSet())));
-        accessLogDto.encryptionKeys = encryptionKeys;
-        return accessLogDto;
+        accessLog.encryptionKeys = encryptionKeys;
+        return accessLog;
       }
-    };
+    }
 
-    final DecryptedAccessLogDto decryptedAccessLogDto = await getEncryptedAccessLog(accessLogDto);
+    ;
+
+    final DecryptedAccessLogDto decryptedAccessLogDto = await getDecryptedAccessLogWithKeys(accessLogDto);
     final Set<String> keys = await this.crypto.decryptEncryptionKeys(myId, decryptedAccessLogDto.encryptionKeys);
     final String stringKey = await Stream<String>.fromIterable(keys).first;
     final Uint8List byteArrayKey = Uint8List.fromList(stringKey.codeUnits);
