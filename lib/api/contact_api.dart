57c2e3885bd8e4d3cae76a2b95740d0f2faffeb1
Clément Vandendaelen <cv@taktik.com>
2022-02-16 10:49:04 +0100

    Added matchServicesBy

--- lib/api/contact_api.dart
+++ lib/api/contact_api.dart
@@ -1651,12 +1651,73 @@ class ContactApi {
     // FormatException when trying to decode an empty string.
     if (response.body.isNotEmpty && response.statusCode != HttpStatus.noContent) {
       final responseBody = await _decodeBodyBytes(response);
-      return (await apiClient.deserializeAsync(responseBody, 'List<ContactDto>') as List)
-        .cast<ContactDto>()
-        .toList();
-
+      return (await apiClient.deserializeAsync(responseBody, 'List<ContactDto>') as List).cast<ContactDto>().toList();
     }
     return null;
+  }
+
+  /// Get ids of services matching the provided filter for the current user
+  ///
+  /// Note: This method returns the HTTP [Response].
+  ///
+  /// Parameters:
+  ///
+  /// * [AbstractFilterDtoService] abstractFilterDtoService (required):
+  Future<Response> rawMatchServicesByWithHttpInfo(
+    AbstractFilterDtoService abstractFilterDtoService,
+  ) async {
+    // Verify required params are set.
+    if (abstractFilterDtoService == null) {
+      throw ApiException(HttpStatus.badRequest, 'Missing required param: abstractFilterDtoService');
+    }
+
+    // ignore: prefer_const_declarations
+    final path = r'/rest/v2/contact/service/match';
+
+    // ignore: prefer_final_locals
+    Object postBody = abstractFilterDtoService;
+
+    final queryParams = <QueryParam>[];
+    final headerParams = <String, String>{};
+    final formParams = <String, String>{};
+
+    const authNames = <String>[];
+    const contentTypes = <String>['application/json'];
+
+    return apiClient.invokeAPI(
+      path,
+      'POST',
+      queryParams,
+      postBody,
+      headerParams,
+      formParams,
+      contentTypes.isEmpty ? null : contentTypes[0],
+      authNames,
+    );
+  }
+
+  /// Get ids of services matching the provided filter for the current user
+  ///
+  /// Parameters:
+  ///
+  /// * [AbstractFilterDtoService] abstractFilterDtoService (required):
+  Future<List<String>> rawMatchServicesBy(
+    AbstractFilterDtoService abstractFilterDtoService,
+  ) async {
+    final response = await rawMatchServicesByWithHttpInfo(
+      abstractFilterDtoService,
+    );
+    if (response.statusCode >= HttpStatus.badRequest) {
+      throw ApiException(response.statusCode, await _decodeBodyBytes(response));
+    }
+    // When a remote server returns no body with a status of 204, we shall not decode it.
+    // At the time of writing this, `dart:convert` will throw an "Unexpected end of input"
+    // FormatException when trying to decode an empty string.
+    if (response.body != null && response.statusCode != HttpStatus.noContent) {
+      final responseBody = await _decodeBodyBytes(response);
+      return (await apiClient.deserializeAsync(responseBody, 'List<String>') as List).cast<String>().toList(growable: false);
+    }
+    return Future<List<String>>.value();
   }
 
   /// Update delegations in healthElements.
