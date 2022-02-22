e175beea037d984b63963c173541b3db59b36413
Antoine Duchâteau <ad@taktik.com>
2022-02-22 22:13:08 +0100

    Do not require key if there is nothing to encrypt for patient

--- lib/extended_api/patient_api.dart
+++ lib/extended_api/patient_api.dart
@@ -99,33 +99,38 @@ extension PatientCryptoConfig on CryptoConfig<DecryptedPatientDto, PatientDto> {
   }
 
   Future<PatientDto> encryptPatient(String dataOwnerId, Set<String> delegations, DecryptedPatientDto patient) async {
-    var eks = patient.encryptionKeys;
-    Uint8List? secret;
-    if (!eks.entries.any((s) => s.value.isNotEmpty)) {
-      secret = Uint8List.fromList(List<int>.generate(32, (i) => random.nextInt(256)));
-      final secretForDelegates = await Future.wait((<String>{...delegations, dataOwnerId}).map((String d) async =>
-          Tuple2(
-              d, await this.crypto.encryptAESKeyForHcp(dataOwnerId, d, patient.id, secret!.toHexString())
-          )));
-
-      eks = {...eks, ...Map.fromEntries(secretForDelegates.map((t) =>
-          MapEntry(t.item1, <DelegationDto>{DelegationDto(
-              owner: dataOwnerId, delegatedTo: t.item1, key: t.item2
-          )})))};
-    } else {
-      secret = IterableUtils((await this.crypto.decryptEncryptionKeys(dataOwnerId, patient.encryptionKeys))).firstOrNull()?.formatAsKey().fromHexString();
-      if (secret == null) {
-        throw FormatException("Cannot get encryption key for ${patient.id} and hcp $dataOwnerId");
-      }
-    }
-
     var t = await this.marshaller(patient);
 
     PatientDto sanitizedPatient = t.item1;
     final Uint8List? marshalledData = t.item2;
 
-    sanitizedPatient.encryptionKeys = eks;
-    sanitizedPatient.encryptedSelf = marshalledData != null ? base64.encoder.convert(marshalledData.encryptAES(secret)) : null;
+
+    if (marshalledData != null) {
+      var eks = patient.encryptionKeys;
+      Uint8List? secret;
+
+      if (!eks.entries.any((s) => s.value.isNotEmpty)) {
+        secret = Uint8List.fromList(List<int>.generate(32, (i) => random.nextInt(256)));
+        final secretForDelegates = await Future.wait((<String>{...delegations, dataOwnerId}).map((String d) async =>
+            Tuple2(
+                d, await this.crypto.encryptAESKeyForHcp(dataOwnerId, d, patient.id, secret!.toHexString())
+            )));
+
+        eks = {...eks, ...Map.fromEntries(secretForDelegates.map((t) =>
+            MapEntry(t.item1, <DelegationDto>{DelegationDto(
+                owner: dataOwnerId, delegatedTo: t.item1, key: t.item2
+            )})))};
+        sanitizedPatient.encryptionKeys = eks;
+      } else {
+        secret = IterableUtils((await this.crypto.decryptEncryptionKeys(dataOwnerId, patient.encryptionKeys))).firstOrNull()?.formatAsKey().fromHexString();
+        if (secret == null) {
+          throw FormatException("Cannot get encryption key for ${patient.id} and hcp $dataOwnerId");
+        }
+      }
+      sanitizedPatient.encryptedSelf = base64.encoder.convert(marshalledData.encryptAES(secret));
+    } else {
+      sanitizedPatient.encryptedSelf = null;
+    }
 
     return sanitizedPatient;
   }
